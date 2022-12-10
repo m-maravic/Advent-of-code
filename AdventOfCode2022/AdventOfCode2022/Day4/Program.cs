959645966b497830f0519ce15182040690b64f8a
m-maravic <milica095@gmail.com>
2022-12-10 21:32:20 +0100

    Day 4

---
+++ AdventOfCode2022/AdventOfCode2022/Day4/Program.cs
@@ -0,0 +1,35 @@
+﻿int counter = 0;
+
+foreach (string line in File.ReadLines(AppDomain.CurrentDomain.BaseDirectory + "/input.txt"))
+{
+    var ranges = line.Split(",");
+    var rangeOne = ranges[0].Split("-").Select(c => Int32.Parse(c)).ToList();
+    var rangeTwo = ranges[1].Split("-").Select(c => Int32.Parse(c)).ToList();
+
+    if (rangeOne[0] <= rangeTwo[0] && rangeOne[1] >= rangeTwo[1] ||
+        rangeTwo[0] <= rangeOne[0] && rangeTwo[1] >= rangeOne[1])
+    {
+        counter++;
+    }
+}
+Console.WriteLine($"Part one result: {counter}");
+
+counter = 0;
+
+foreach (string line in File.ReadLines(AppDomain.CurrentDomain.BaseDirectory + "/input.txt"))
+{
+    var ranges = line.Split(",");
+    var rangeOne = ranges[0].Split("-").Select(c => Int32.Parse(c)).ToList();
+    var rangeTwo = ranges[1].Split("-").Select(c => Int32.Parse(c)).ToList();
+    
+
+    if (rangeOne[0] <= rangeTwo[0] && !(rangeOne[1] < rangeTwo[0]) ||
+        rangeTwo[0] <= rangeOne[0] && !(rangeTwo[1] < rangeOne[0]))
+    {
+        counter++;
+    }
+}
+
+Console.WriteLine($"Part two result: {counter}");
+
+
