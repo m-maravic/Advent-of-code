1fe5f7c1d539198cd0bf45d3069c7a850448c04c
m-maravic <milica095@gmail.com>
2022-12-10 15:57:14 +0100

    Advent

---
+++ AdventOfCode2022/Day1/Day1/Program.cs
@@ -0,0 +1,44 @@
+﻿var currentSum = 0;
+var maxSum = 0;
+
+foreach (string line in File.ReadLines(AppDomain.CurrentDomain.BaseDirectory + "/input.txt"))
+{
+    var isNumber = int.TryParse(line, out var currentNumber);
+    
+    if (isNumber)
+    {
+        currentSum += currentNumber;
+        if (currentSum > maxSum)
+        {
+            maxSum = currentSum;
+        }
+    }
+    else
+    {
+        currentSum = 0;
+    }
+}
+
+Console.WriteLine($"First part result: {maxSum}");
+
+
+var caloriesPerElf = new List<int>();
+currentSum = 0;
+
+foreach (string line in File.ReadLines(AppDomain.CurrentDomain.BaseDirectory + "/input.txt"))
+{
+    var isNumber = int.TryParse(line, out var currentNumber);
+
+    if (isNumber)
+    {
+        currentSum += currentNumber;
+    }
+    else
+    {
+        caloriesPerElf.Add(currentSum);
+        currentSum = 0;
+    }
+}
+
+caloriesPerElf.Sort();
+Console.WriteLine($"Part two result:{caloriesPerElf[^1] + caloriesPerElf[^2] + caloriesPerElf[^3]}");
