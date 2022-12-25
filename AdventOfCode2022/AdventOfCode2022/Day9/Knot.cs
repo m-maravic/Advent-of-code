d7d455c79d36ff55b29f9e5af0dc7fcdc9fc2054
m-maravic <milica095@gmail.com>
2022-12-25 15:36:41 +0100

    Day 9 part one

---
+++ AdventOfCode2022/AdventOfCode2022/Day9/Knot.cs
@@ -0,0 +1,40 @@
+﻿namespace Day9;
+
+public class Knot
+{
+    public int X { get; private set; }
+    public int Y { get; private set; }
+
+    public Knot(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public void Move(int x, int y)
+    {
+        this.X = x;
+        this.Y = y;
+    }
+
+    public bool IsTouching(int x, int y)
+    {
+        if (x == this.X - 1 && (y == this.Y - 1 || y == this.Y || y == this.Y + 1))
+        {
+            return true;
+        }
+
+        if (x == this.X && (y == this.Y - 1 || y == this.Y || y == this.Y + 1))
+        {
+            return true;
+        }
+
+        if (x == this.X + 1 && (y == this.Y - 1 || y == this.Y || y == this.Y + 1))
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
+
---
+++ AdventOfCode2022/AdventOfCode2022/Day9/Program.cs
@@ -0,0 +1,66 @@
+﻿using Day9;
+
+var head = new Knot(5, 1);
+var tail = new Knot(5, 1);
+
+var positions = new List<Tuple<int, int>>() { new Tuple<int, int>(5, 1) };
+
+foreach (string motion in File.ReadLines(AppDomain.CurrentDomain.BaseDirectory + "/input.txt"))
+{
+    var direction = motion.Split(' ')[0];
+    var steps = Int32.Parse(motion.Split(' ')[1]);
+
+    switch (direction)
+    {
+        case "U":
+            for (int i = 0; i < steps; i++)
+            {
+                head.Move(head.X - 1, head.Y);
+                if (!tail.IsTouching(head.X, head.Y))
+                {
+                    tail.Move(head.X + 1, head.Y);
+                    positions.Add(new Tuple<int, int>(tail.X, tail.Y));
+                }
+            }
+            break;
+        case "D":
+            for (int i = 0; i < steps; i++)
+            {
+                head.Move(head.X + 1, head.Y);
+                if (!tail.IsTouching(head.X, head.Y))
+                {
+                    tail.Move(head.X - 1, head.Y);
+                    positions.Add(new Tuple<int, int>(tail.X, tail.Y));
+                }
+            }
+            break;
+        case "L":
+            for (int i = 0; i < steps; i++)
+            {
+                head.Move(head.X, head.Y - 1);
+                if (!tail.IsTouching(head.X, head.Y))
+                {
+                    tail.Move(head.X, head.Y +1);
+                    positions.Add(new Tuple<int, int>(tail.X, tail.Y));
+                }
+            }
+            break;
+        case "R":
+            for (int i = 0; i < steps; i++)
+            {
+                head.Move(head.X, head.Y + 1);
+                if (!tail.IsTouching(head.X, head.Y))
+                {
+                    tail.Move(head.X, head.Y - 1);
+                    positions.Add(new Tuple<int, int>(tail.X, tail.Y));
+                }
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+var partOne = positions.Distinct().Count();
+
+Console.WriteLine("Part one: " + partOne);
